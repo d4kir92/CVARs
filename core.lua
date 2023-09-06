98657a640429fcc32875433886da0d07b7dbd9ae
D4KiR <d4kir92@gmail.com>
2023-09-06 09:41:15 +0200

    v1.0.5

--- core.lua
+++ core.lua
@@ -1,10 +1,21 @@
 local _, CVARs = ...
+local CVARS_DEBUG = false
 
-function CVARs:OnInitialize()
+function CVARs:OnInitialize(event, ...)
 	SetCVar("autoLootDefault", 1)
 	SetCVar("enableFloatingCombatText", 1)
+	SetCVar("floatingCombatTextCombatHealing", 1)
+
+	if event == "CVAR_UPDATE" then
+		print(...)
+	end
 end
 
 local f = CreateFrame("FRAME")
 f:RegisterEvent("PLAYER_LOGIN")
+
+if CVARS_DEBUG then
+	f:RegisterEvent("CVAR_UPDATE")
+end
+
 f:SetScript("OnEvent", CVARs.OnInitialize)
