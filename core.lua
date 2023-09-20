52a708172b0730320f187e13223f678e3bb55688
D4KiR <d4kir92@gmail.com>
2023-09-20 11:12:05 +0200

    v1.0.7

--- core.lua
+++ core.lua
@@ -1,12 +1,10 @@
 local _, CVARs = ...
 local CVARS_DEBUG = false
-
 function CVARs:OnInitialize(event, ...)
-	SetCVar("autoLootDefault", 1)
-	SetCVar("enableFloatingCombatText", 1)
-	SetCVar("floatingCombatTextCombatHealing", 1)
-	SetCVar("xpBarText", 1)
-
+	SetCVar("autoLootDefault", 1) -- Fast looting
+	SetCVar("enableFloatingCombatText", 1) -- Combattext
+	SetCVar("floatingCombatTextCombatHealing", 1) -- Combattext: Healing
+	SetCVar("xpBarText", 1) -- Show XP Text
 	if event == "CVAR_UPDATE" then
 		print(...)
 	end
@@ -14,7 +12,6 @@ end
 
 local f = CreateFrame("FRAME")
 f:RegisterEvent("PLAYER_LOGIN")
-
 if CVARS_DEBUG then
 	f:RegisterEvent("CVAR_UPDATE")
 end
