0d2f199d108bde8efff4a34135af4457acb3ab8a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-05-27 09:46:35 +0200

    v1.2.65

--- core.lua
+++ core.lua
@@ -79,7 +79,7 @@ end
 
 function CVARs:OnInitialize(event, ...)
 	if event == "PLAYER_LOGIN" then
-		CVARs:SetVersion(134063, "1.2.64")
+		CVARs:SetVersion(134063, "1.2.65")
 		for i = 1, 100 do
 			if GetCVar("nameplateMaxDistance", i) ~= nil then
 				local currentDist = tonumber(GetCVar("nameplateMaxDistance", i))
