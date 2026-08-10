7d67de25f1e12881c6c048203ac1b760814868af
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-08-10 23:24:15 +0200

    v1.2.105

--- core.lua
+++ core.lua
@@ -39,7 +39,7 @@ function CVARs:OnInitialize(event, ...)
 	if event == "ADDON_LOADED" then
 		local addonName = select(1, ...)
 		if addonName == AddonName then
-			CVARs:SetVersion(134063, "1.2.104")
+			CVARs:SetVersion(134063, "1.2.105")
 			for i = 1, 100 do
 				if GetCVar("nameplateMaxDistance") ~= nil then
 					local currentDist = tonumber(GetCVar("nameplateMaxDistance"))
